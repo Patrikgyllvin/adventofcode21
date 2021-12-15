186f462a855aa41d1bce518a348854cd9383d246
Patrik Gyllvin <patrikgyllvin@icloud.com>
2021-12-15 23:38:04 +0100

    Add almost ready day 15

---
+++ src/15/15.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+#include <sstream>
+
+#include <string>
+#include <vector>
+#include <queue>
+
+#include <functional>
+#include <limits>
+
+#include "lib.hpp"
+
+auto parse( const std::string& input, int& w, int& h )
+{
+    std::vector< int > board;
+    
+    w = input.find( '\n' );
+    for( const auto& c : input )
+    {
+        if( c == '\n' ) continue;
+
+        board.push_back( c - '0' );
+    }
+    h = board.size() / w;
+    
+    return board;
+}
+
+auto tileParse( const std::string& input, int num, int& w, int& h )
+{
+    std::string tmp, full;
+    
+    for( int i = 0; i != num; ++i )
+        tmp += input;
+    
+    std::string line;
+    std::stringstream sstream{ tmp };
+    int inc = 0;
+    while( std::getline( sstream, line ) )
+    {
+        for( int i = 0; i != num; ++i )
+        {
+            for( std::size_t j = 0; j != line.size(); ++j )
+            {
+                int val = ( ( line[ j ] - '0' ) + i + inc ) % 10;
+                line[ j ] = val == 0 ? 1 + '0' : val + '0';
+            }
+            full += line;
+        }
+        full += '\n';
+        inc++;
+    }
+    
+    std::cout << full;
+
+    return parse( full, w, h );
+}
+
+std::vector< int > getDirections( int curr, int w, int h )
+{
+    std::vector< int > dirs;
+    int x = curr % w, y = curr / w;
+
+    if( x != w - 1 )
+        dirs.push_back( curr + 1 );
+    if( x != 0 )
+        dirs.push_back( curr - 1 );
+    if( y != h - 1 )
+        dirs.push_back( curr + w );
+    if( x != 0 )
+        dirs.push_back( curr - w );
+    
+    return dirs;
+}
+
+std::vector< int > dijkstra( const std::vector< int >& board, int w, int h )
+{
+    std::vector< int > dist( board.size(), std::numeric_limits< int >::max() );
+    std::priority_queue<
+        std::pair< int, int >,
+        std::vector< std::pair< int, int > >,
+        std::greater< std::pair< int, int > > > q;
+
+    dist[ 0 ] = 0;
+    q.emplace( 0, 0 );
+
+    while( !q.empty() )
+    {
+        int n = q.top().second;
+        q.pop();
+
+        std::vector< int > dirs = getDirections( n, w, h );
+        for( const int& dir : dirs )
+        {
+            int distance = dist[ n ] + board[ dir ];
+            if( distance < dist[ dir ] )
+            {
+                dist[ dir ] = distance;
+                q.emplace( distance, dir );
+            }
+        }
+    }
+    
+    return dist;
+}
+
+unsigned long long partOne( const std::string& input )
+{
+    int w, h;
+    std::vector< int > board = parse( input, w, h );
+
+    return dijkstra( board, w, h )[ board.size() - 1 ];
+}
+
+unsigned long long partTwo( const std::string& input )
+{
+    int w, h;
+    std::vector< int > board = tileParse( input, 5, w, h );
+
+    return dijkstra( board, w, h )[ board.size() - 1];
+}
